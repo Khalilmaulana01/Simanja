e97d661081b429187ccbb8da709544d441c6e8e6
aimi <rihhadatul00@gmail.com>
2021-08-06 17:51:22 +0700

    Splash dan Login Selesai, Lupa Psswd On Progress

--- Simanja/app/src/main/java/com/example/simanja/MainActivity.java
+++ Simanja/app/src/main/java/com/example/simanja/MainActivity.java
@@ -9,15 +9,16 @@ import android.os.Handler;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     //variableanimasi
     ImageView image;
     TextView logo;
-
 
     //waktu
     public static int SPLASH_SCREEN = 5000;
@@ -27,6 +28,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
+        //panggil button lupa password
+//        Button lupabutton = findViewById(R.id.lupabutton);
+//
+//        lupabutton.setOnClickListener(v -> {
+//
+//            Intent intent = new Intent(this, forget_password.class);
+//
+//            startActivity(intent);
+//                    });
+
+
 
         //Hooks
         image = findViewById(R.id.imageView);
@@ -44,9 +58,18 @@ public class MainActivity extends AppCompatActivity {
                 pairs[1] = new Pair<View, String>(logo, "logo_text");
 
 
-                ActivityOptions Options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                ActivityOptions Options = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    Options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                }
                 startActivity(intent,Options.toBundle());
             }
+
         },SPLASH_SCREEN );
+//        lupabutton.setOnClickListener(v -> {
+//
+//            Intent intent = new Intent(this,forget_password.class);
+//            startActivity(intent);
+        };
     }
-}
+
