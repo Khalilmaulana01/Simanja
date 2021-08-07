a264d2d59d97453570dc51c91b5a34f1dd306bcd
prometheus <khalilalkhabasan123@gmail.com>
2021-08-07 15:02:39 +0700

    perbaiki login page

--- Simanja/app/src/main/java/com/example/simanja/Login.java
+++ Simanja/app/src/main/java/com/example/simanja/Login.java
@@ -7,10 +7,24 @@ import android.view.WindowManager;
 
 public class Login extends AppCompatActivity {
 
+    Button lupabutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+    
+    lupabutton = findViewById(R.id.lupabutton);
+    lupabutton.setOnClickListener (new View.OnclickListener(){
+        @Override
+        public void onClick(View view){
+
+            Intent intent =new Intent (Login.this, Lupapassword.class);
+            startActivity(intent);
+        }
+
+    });
+    
     }
 }
